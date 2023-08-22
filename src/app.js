481fe5cce8d9d868daaeffb11c296d84cc1b73b2
SatganzDevs <satganzdevs@gmail.com>
2023-08-22 12:54:01 +0000

    the latest

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import { fileURLToPath } from "url";
+import path from "path";
 
-import * as middleware from "./utils/middleware.js";
+
 import helloRoute from "./routes/helloRouter.js";
 import apiRoute from "./routes/api.js";
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'src/views')));
+
 
 // parse json request body
 app.use(express.json());
@@ -17,16 +27,17 @@ app.use(cors());
 // request logger middleware
 app.use(morgan("tiny"));
 
-// healthcheck endpoint
 app.get("/", (req, res) => {
-  res.status(200).send({ status: "ok" });
+  const targetURL = "https://docs.api.satganzdevs.cloud"; 
+  res.redirect(targetURL);
 });
+
 
 app.use("/hello", helloRoute);
 app.use("/api", apiRoute);
 
-// custom middleware
-app.use(middleware.unknownEndpoint);
-app.use(middleware.errorHandler);
+// // custom middleware
+// app.use(middleware.unknownEndpoint);
+// app.use(middleware.errorHandler);
 
 export default app;
